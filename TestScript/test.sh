a8ed9b730a19a3b76ccb38854b6618a0363f5bc9
liyingpeng <liyingpeng@bytedance.com>
2019-05-04 16:18:08 +0800

    add my information

--- TestScript/test.sh
+++ TestScript/test.sh
@@ -1,6 +1,20 @@
 # !bin/bash
 
-Param1=$1
-Para2=$2
+# Date：   16:29 2012-3-30
+# Author： Created by liyingpeng
+# Blog:http://oldboy.blog.51cto.com
+# Description：This scripts function is.....
+# Version：1.1
+a=192.168.1.2
+b='192.168.1.2'
+c="192.168.1.2"
+echo "a=$a"
+echo "b=$b"
+echo "c=${c}"
 
-echo $Param1	
+a=192.168.1.2-$a
+b='192.168.1.2-$a'
+c="192.168.1.2-$a"
+echo "a=$a"
+echo "b=$b"
+echo "c=${c}"
